e97420a165c293a5db92b317e1c8f85a9c41b3f6
Alan <127437916+AlanRodriguess@users.noreply.github.com>
2026-08-12 14:43:32 -0300

    Rename main2.cpp to Aula02/main2.cpp

--- Aula02/main2.cpp
+++ Aula02/main2.cpp
@@ -6,13 +6,13 @@ int main() {
 	setlocale(LC_ALL, "Portuguese");
 	int numero;
 	
-	cout << "Digite um número: ";
+	cout << "Digite um nÃºmero: ";
 	cin >> numero;
 	
 	if(numero >= 10 && numero <= 20){
-		cout << "O número está no intervalo de 10 a 20. " << endl;
+		cout << "O nÃºmero estÃ¡ no intervalo de 10 a 20. " << endl;
 	}else{
-		cout << "O número está fora do intervalo de 10 a 20." << endl;
+		cout << "O nÃºmero estÃ¡ fora do intervalo de 10 a 20." << endl;
 	}
 	
 	return 0;
