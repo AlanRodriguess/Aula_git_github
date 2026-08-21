4553e8024988700c2167a1c903da1b37f83b73e0
Alan <127437916+AlanRodriguess@users.noreply.github.com>
2026-08-20 22:47:29 -0300

    Rename atividade2.cpp to Aula03/atividade2.cpp

--- Aula03/atividade2.cpp
+++ Aula03/atividade2.cpp
@@ -7,7 +7,7 @@ int main() {
 	
 	double salario, salarioAnual, result;
 	
-	cout << "Digite seu salário mensal: ";
+	cout << "Digite seu salÃ¡rio mensal: ";
 	cin >> salario;
 	
 	salarioAnual = salario * 12;
@@ -17,11 +17,11 @@ int main() {
 	}else if(salarioAnual > 20000 && salarioAnual <= 50000){
 		result = salarioAnual * 0.10;
 		result = salarioAnual - result;
-		cout << "Seu salário anual com o valor de imposto de renda: " << result << endl;
+		cout << "Seu salÃ¡rio anual com o valor de imposto de renda: " << result << endl;
 	}else{
 		result = salarioAnual * 0.20;
 		result = salarioAnual - result;
-		cout << "Seu salário anual com o valor de imposto de renda: " << result << endl;
+		cout << "Seu salÃ¡rio anual com o valor de imposto de renda: " << result << endl;
 	}
 	
 	 	
